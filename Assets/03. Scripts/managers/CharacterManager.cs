3e738e1d558601869118ac11b9be49ceed3d3b45
kimeunseo <kimeunseo.work@gmail.com>
2025-11-25 11:27:23 +0900

    feat: ValueBarController 구현(HP,MP,EXP)

--- Assets/03. Scripts/managers/CharacterManager.cs
+++ Assets/03. Scripts/managers/CharacterManager.cs
@@ -1,11 +1,173 @@
-﻿/// <summary>
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Stat
+{
+    Hp,
+    MaxHp,
+    Mp,
+    MaxMp,
+    Exp,
+    MaxExp
+}
+
+/// <summary>
 /// 캐릭터 정보 관리.
 /// 플레이어 스탯 관리, 레벨업, 스킬 관리.
 /// </summary>
 public class CharacterManager : Manager
 {
+    #region Fields
+    int hp;
+    int maxHp;
+    int mp;
+    int maxMp;
+    int exp;
+    int maxExp;
+    #endregion
+
+    #region Properties
+    public int Hp
+    {
+        get => hp;
+        private set => hp = Mathf.Clamp(value, 0, maxHp);
+    }
+    public int Mp
+    {
+        get => mp;
+        private set => mp = Mathf.Clamp(value, 0, maxMp);
+    }
+    public int Exp
+    {
+        get => exp;
+        private set => exp = Mathf.Clamp(value, 0, maxExp);
+    }
+    public int MaxHp
+    {
+        get => maxHp;
+        private set => maxHp = value;
+    }
+    public int MaxMp
+    {
+        get => maxMp;
+        private set => maxMp = value;
+    }
+    public int MaxExp
+    {
+        get => maxExp;
+        private set => maxExp = value;
+    }
+    #endregion
+
+    public Dictionary<Stat, int> Stats;
+    public Dictionary<Stat, Action<int, int>> StatEvents;
+
     public override void Init()
     {
-        throw new System.NotImplementedException();
+        MaxHp = 100;
+        MaxMp = 200;
+        MaxExp = 800;
+
+        Hp = 90;
+        Mp = 180;
+        Exp = 100;
+
+        Stats = new()
+        {
+            {Stat.Hp, Hp},
+            {Stat.MaxHp, MaxHp},
+            {Stat.Mp, Mp},
+            {Stat.MaxMp, MaxMp},
+            {Stat.Exp, Exp},
+            {Stat.MaxExp, MaxExp},
+        };
+
+        StatEvents = new()
+        {
+            {Stat.Hp, delegate { }},
+            {Stat.Mp, delegate { }},
+            {Stat.Exp, delegate { }},
+        };
     }
+
+    #region In/Decrease Stats
+
+    #region Hp/MaxHp
+    public void IncreaseHp(int amount)
+    {
+        Hp += amount;
+        StatEvents[Stat.Hp]?.Invoke(Hp, MaxHp);
+    }
+
+    public void DecreaseHp(int amount)
+    {
+        Hp -= amount;
+        StatEvents[Stat.Hp]?.Invoke(Hp, MaxHp);
+    }
+
+    public void IncreaseMaxHp(int amount)
+    {
+        MaxHp += amount;
+        StatEvents[Stat.Hp]?.Invoke(Hp, MaxHp);
+    }
+
+    public void DecreaseMaxHp(int amount)
+    {
+        MaxHp -= amount;
+        StatEvents[Stat.Hp]?.Invoke(Hp, MaxHp);
+    }
+    #endregion
+
+    #region Mp/MaxMp
+    public void IncreaseMp(int amount)
+    {
+        Mp += amount;
+        StatEvents[Stat.Mp]?.Invoke(Mp, MaxMp);
+    }
+
+    public void DecreaseMp(int amount)
+    {
+        Mp -= amount;
+        StatEvents[Stat.Mp]?.Invoke(Mp, MaxMp);
+    }
+    public void IncreaseMaxMp(int amount)
+    {
+        MaxMp += amount;
+        StatEvents[Stat.Mp]?.Invoke(Mp, MaxMp);
+    }
+
+    public void DecreaseMaxMp(int amount)
+    {
+        MaxMp -= amount;
+        StatEvents[Stat.Mp]?.Invoke(Mp, MaxMp);
+    }
+    #endregion
+
+    #region Exp/MaxExp
+    public void IncreaseExp(int amount)
+    {
+        Exp += amount;
+        StatEvents[Stat.Exp]?.Invoke(Exp, MaxExp);
+    }
+
+    public void DecreaseExp(int amount)
+    {
+        Exp -= amount;
+        StatEvents[Stat.Exp]?.Invoke(Exp, MaxExp);
+    }
+    public void IncreaseMaxExp(int amount)
+    {
+        MaxExp += amount;
+        StatEvents[Stat.Exp]?.Invoke(Exp, MaxExp);
+    }
+
+    public void DecreaseMaxExp(int amount)
+    {
+        MaxExp -= amount;
+        StatEvents[Stat.Exp]?.Invoke(Exp, MaxExp);
+    }
+    #endregion
+
+    #endregion
 }
---
+++ Assets/03. Scripts/ui/ValueBarController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+/// <summary>
+/// Image 컴포넌트를 기반으로 현재 값과 최대 값을 가지고
+/// 시각적 정보를 제공하는 오브젝트의 컨트롤러 스크립트
+/// </summary>
+public class ValueBarController : MonoBehaviour
+{
+    [Header("Component")]
+    [SerializeField] Image image;
+
+    [Header("Stats")]
+    [SerializeField] Stat type;
+    [SerializeField] Stat maxType;
+
+    CharacterManager characterManager;
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        image = GameObject.Find("fill").GetComponent<Image>();
+    }
+#endif
+
+    #region Unity LifeCycle
+    private void Awake()
+    {
+        characterManager = ManagerRoot.Instance.CharacterManager;
+
+        if(characterManager == null )
+        {
+            Debug.Log($"characterManager is null");
+            return;
+        }
+    }
+
+    private void OnEnable()
+    {
+        // 유효하지 않은 키
+        Debug.Assert(
+            characterManager.StatEvents.ContainsKey(type),
+            $"{type} is not Valid"
+        );
+
+        // 이벤트가 없는 경우
+        if (characterManager.StatEvents.ContainsKey(type)
+            && characterManager.StatEvents[type] == null
+        )
+        {
+            Debug.Log($"StatEvents is null");
+            return;
+        }
+
+        characterManager.StatEvents[type] += CharacterManager_ValueChanged;
+    }
+
+    private void OnDisable()
+    {
+        characterManager.StatEvents[type] -= CharacterManager_ValueChanged;
+    }
+
+    private void Start()
+    {
+        if (image == null)
+        {
+            Debug.Log($"[{name}] image is null so, Find component already");
+            image = GameObject.Find("fill").GetComponent<Image>();
+
+            Debug.Assert(image != null, $"fill image is not Exist");
+        }
+
+        image.fillAmount = (float)characterManager.Stats[type] / characterManager.Stats[maxType];
+    }
+
+    #endregion
+
+    private void CharacterManager_ValueChanged(int value, int maxValue)
+    {
+        image.fillAmount = (float)value / maxValue;
+    }
+}
